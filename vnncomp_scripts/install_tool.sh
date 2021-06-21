017f41fa5070d6416906dfd23f6395062ee50433
Wei-TianHao <phi.wth@gmail.com>
2021-06-20 23:11:11 -0400

    update scripts

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -3,8 +3,7 @@ apt-get install wget -y
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz
 tar -xvzf julia-1.6.1-linux-x86_64.tar.gz
 cp -r julia-1.6.1 /opt/
-ln -s /opt/julia-1.6.0/bin/julia /usr/local/bin/julia
-export PATH="$PATH:/opt/julia-1.6.0/bin/julia"
+ln -s /opt/julia-1.6.1/bin/julia /usr/local/bin/julia
 apt-get install build-essential -y
 apt-get install git -y
 apt-get install python3 -y
