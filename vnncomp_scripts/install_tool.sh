ec7027b116dcd5d31940f8521f05253148b92505
Wei-TianHao <phi.wth@gmail.com>
2021-06-27 23:34:01 -0400

    add sudo

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -1,14 +1,15 @@
 apt-get update -y
-apt-get install wget -y
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz
-tar -xvzf julia-1.6.1-linux-x86_64.tar.gz
-cp -r julia-1.6.1 /opt/
-ln -s /opt/julia-1.6.1/bin/julia /usr/local/bin/julia
-apt-get install build-essential -y
-apt-get install git -y
-apt-get install python3 -y
-apt-get install pip -y
-apt-get install psmisc
+apt-get -y install sudo
+sudo apt-get install wget -y
+sudo wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz
+sudo tar -xvzf julia-1.6.1-linux-x86_64.tar.gz
+sudo cp -r julia-1.6.1 /opt/
+sudo ln -s /opt/julia-1.6.1/bin/julia /usr/local/bin/julia
+sudo apt-get install build-essential -y
+sudo apt-get install git -y
+sudo apt-get install python3 -y
+sudo apt-get install pip -y
+sudo apt-get install psmisc
 
 cd NeuralVerification.jl
 echo '
