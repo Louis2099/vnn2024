012f5dcd9a95612b403af88df49655bb5f850c2f
Wei-TianHao <phi.wth@gmail.com>
2022-02-21 01:52:19 -0500

    add min invasive mode

--- src/optimization/nnDyn.jl
+++ src/optimization/nnDyn.jl
@@ -2,7 +2,7 @@
     optimizer = CPLEX.Optimizer
 end
 
-function solve(solver::NNDynTrack, problem::TrackingProblem, start_values=nothing)
+function solve(solver::NNDynTrack, problem::TrackingProblem, start_values=nothing; u_ref = nothing, xu_init = nothing)
     model = Model(solver)
     set_silent(model)
     set_optimizer_attribute(model, "CPX_PARAM_EPOPT", 1e-8)
@@ -19,14 +19,18 @@ function solve(solver::NNDynTrack, problem::TrackingProblem, start_values=nothin
     add_set_constraint!(model, problem.output, last(z))
     encode_network!(model, problem.network, BoundedMixedIntegerLP())
 
-    # o = symbolic_abs((last(z) - problem.output_ref).*problem.output_cost)
     
+    # o = symbolic_abs((last(z) - problem.output_ref).*problem.output_cost)
     d = (last(z) - problem.output_ref)
-    o = dot(d, d.*problem.output_cost)
+    o = isnothing(u_ref) ? dot(d, d.*problem.output_cost) : dot(first(z)[5:6] - u_ref, first(z)[5:6] - u_ref)
     
     @objective(model, Min, o)
 
     isnothing(start_values) || set_start_value.(all_variables(model), start_values)
+
+    if !isnothing(xu_init)
+        set_start_value.(first(z), xu_init)
+    end
 
     optimize!(model)
 
